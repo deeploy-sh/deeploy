395d5be2f3e8ab2921e2fdd828fe7dfb40c096f8
axzilla <hello@axeladrian.com>
2025-11-26 13:36:52 +0400

    chore: clean up app model

--- internal/deeploy/ui/pages/app.go
+++ internal/deeploy/ui/pages/app.go
@@ -10,110 +10,58 @@ import (
 	"github.com/deeploy-sh/deeploy/internal/deeploy/ui/styles"
 )
 
-// /////////////////////////////////////////////////////////////////////////////
-// Types & Messages
-// /////////////////////////////////////////////////////////////////////////////
-
-type HasInputView interface {
-	HasFocusedInput() bool
-}
-
 type app struct {
 	currentPage tea.Model
 	width       int
 	height      int
 }
 
-// /////////////////////////////////////////////////////////////////////////////
-// Constructors
-// /////////////////////////////////////////////////////////////////////////////
-
 func NewApp() app {
-	return app{}
-}
-
-// We wait for window size before creating pages
-func (a app) Init() tea.Cmd {
-	return nil
-}
-
-func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if msg, ok := msg.(tea.KeyMsg); ok {
-		if msg.Type == tea.KeyCtrlC {
-			return a, tea.Quit
-		}
-
-		currentPage := a.currentPage
-		if page, ok := currentPage.(HasInputView); ok && page.HasFocusedInput() {
-			// this disable "q"
-		} else if msg.String() == "q" {
-			return a, tea.Quit
-		}
-
+	return app{
+		currentPage: NewBootstrap(),
 	}
+}
 
+func (m app) Init() tea.Cmd {
+	return m.currentPage.Init()
+}
+
+func (m app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		a.width = msg.Width
-		a.height = msg.Height
-
-		// If no pages yet, create first one
-		if a.currentPage == nil {
-			// config, err := config.LoadConfig()
-			// config, _ := config.LoadConfig()
-			// log.Println(config)
-			var page tea.Model
-
-			page = NewBootstrap()
-			// No config = show login, has config = show dashboard
-			// if err != nil || config.Server == "" || config.Token == "" {
-			// 	page = NewConnectPage()
-			// } else {
-			// 	page = NewDashboard()
-			// }
-
-			// Add first page to stack
-			a.currentPage = page
-
-			// Update page with window size and initialize it
-			updatedPage, cmd := page.Update(msg)
-			a.currentPage = updatedPage
-			return a, tea.Batch(cmd, updatedPage.Init())
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
 		}
 
-		// Update current page's window size
-		currentPage := a.currentPage
-		updatedPage, cmd := currentPage.Update(msg)
-		a.currentPage = updatedPage
-		return a, cmd
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		// return a, cmd
+		// return a, nil
 
 	case messages.ChangePageMsg:
 		newPage := msg.Page
-
-		a.currentPage = newPage
+		m.currentPage = newPage
 
 		// Batch window size and init commands together
 		// This prevents double rendering by ensuring both happen in sequence
-		return a, tea.Batch(
+		return m, tea.Batch(
 			func() tea.Msg {
 				return tea.WindowSizeMsg{
-					Width:  a.width,
-					Height: a.height,
+					Width:  m.width,
+					Height: m.height,
 				}
 			},
-			newPage.Init(),
+			// INFO: do we really need this here?
+			// newPage.Init(),
 		)
+	}
 
 	// All other messages go to current page
-	default:
-		if a.currentPage == nil {
-			return a, nil
-		}
-		currentPage := a.currentPage
-		updatedPage, cmd := currentPage.Update(msg)
-		a.currentPage = updatedPage
-		return a, cmd
-	}
+	currentPage := m.currentPage
+	updatedPage, cmd := currentPage.Update(msg)
+	m.currentPage = updatedPage
+	return m, cmd
 }
 
 type FooterMenuItem struct {
@@ -121,12 +69,8 @@ type FooterMenuItem struct {
 	Desc string
 }
 
-func (a app) View() string {
-	if a.currentPage == nil {
-		return "Loading..."
-	}
-
-	main := a.currentPage.View()
+func (m app) View() string {
+	main := m.currentPage.View()
 
 	footerMenuItems := []FooterMenuItem{
 		{Key: ":", Desc: "menu"},
@@ -146,7 +90,7 @@ func (a app) View() string {
 	}
 
 	footerCard := components.Card(components.CardProps{
-		Width:   a.width,
+		Width:   m.width,
 		Padding: []int{0, 1},
 	}).Render(footer.String())
 
