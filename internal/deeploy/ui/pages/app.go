d8ee4ce4c250e696b673bf901d0670ee7f58b01b
axzilla <hello@axeladrian.com>
2025-12-03 16:32:34 +0400

    fix: remove -1 header height on initial page size msg

--- internal/deeploy/ui/pages/app.go
+++ internal/deeploy/ui/pages/app.go
@@ -135,7 +135,7 @@ func (m app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		pageMsg := tea.WindowSizeMsg{
 			Width:  m.width,
-			Height: m.height - headerHeight,
+			Height: m.height,
 		}
 		var cmd tea.Cmd
 		if m.currentPage == nil {
